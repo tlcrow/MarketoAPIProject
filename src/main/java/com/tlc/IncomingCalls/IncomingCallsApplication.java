deeb7261f5162773965c4abce30b9211647adf25
TLC <pi_lover852@yahoo.com>
2018-03-24 15:51:32 -0600

    project setup

---
+++ src/main/java/com/tlc/IncomingCalls/IncomingCallsApplication.java
@@ -0,0 +1,12 @@
+package com.tlc.IncomingCalls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncomingCallsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncomingCallsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tlc/IncomingCalls/IncomingCallsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tlc.IncomingCalls;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IncomingCallsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
